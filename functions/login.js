58846c01bb879af5afe4677dbd05f1de3fdc7fbd
Abir <samiulabir41@gmail.com>
2024-09-06 12:30:48 +0600

    added new scope

--- functions/login.js
+++ functions/login.js
@@ -6,7 +6,11 @@ exports.handler = async function (event, context) {
   const client_id = process.env.VITE_SPOTIFY_CLIENT_ID;
   const redirect_uri = process.env.VITE_SPOTIFY_REDIRECT_URI;
 
-  const scopes = encodeURIComponent('user-read-private user-read-email');
+  // const scopes = encodeURIComponent('user-read-private user-read-email');
+  const scopes = encodeURIComponent(
+    'user-read-private user-read-email user-follow-read playlist-read-private'
+  );
+
   const authUrl =
     'https://accounts.spotify.com/authorize?' +
     querystring.stringify({
